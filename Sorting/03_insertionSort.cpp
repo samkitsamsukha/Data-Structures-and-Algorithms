7bf907f0898c90dbfb3e8c17c4736f35ab595162
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-01-23 00:57:18 +0530

    Added Insertion sort and Merge Sort

---
+++ Sorting/03_insertionSort.cpp
@@ -0,0 +1,38 @@
+//Insertion Sort - start picking elements from the left and place them in correct order by comparing whether they are larger than the immediately preceding element.
+/*
+    Note on Time Complexity:-
+    - 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter number of array elements :-" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements :-" << endl;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    //INSERTION SORT
+    for(int i=0; i<n; i++){
+        int j=i; //to start inserting elements from the left side. The value of i increases in this loop indicating that the array elements have already been put into the correct places.
+        while(j>0 && arr[j-1]>arr[j]){ //check if immediately preceding element is greater
+            //swapping elements
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+        }
+    }
+
+    //Sorted Array
+    cout << "Sorted Array :-" << endl;
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
---
+++ Sorting/04_mergeSort.cpp
@@ -0,0 +1,76 @@
+// Merge Sort - Divide and Merge
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high)
+{
+    vector<int> temp;
+    //[low...mid]
+    //[mid+1...high]
+    int left = low;
+    int right = mid + 1;
+    while ((left <= mid) && (right <= high))
+    {
+        //pointer approach to check two elements in the first half and second half
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid) //in case of elements remaining in the left half
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high) //in case of elements remaining in the right half
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    if (low == high)
+    {
+        return;
+    }
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid); //Left Divided Array
+    mergeSort(arr, mid+1, high); //Right Divided Array
+    merge(arr, low, mid, high); //Merge both arrays
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements in the array" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n-1);
+
+    cout << "Sorted Array:-" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
