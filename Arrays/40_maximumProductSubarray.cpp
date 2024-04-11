754f69332fef53ca2380577b1a4bf075fed54c78
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-04-12 00:28:37 +0530

    Update to max prod subarray

--- Arrays/40_maximumProductSubarray.cpp
+++ Arrays/40_maximumProductSubarray.cpp
@@ -35,6 +35,12 @@ int optimal(vector<int> nums){
     int suf = 1;
     int maxi = INT_MIN;
     for(int i = 0; i < nums.size(); i++){
+        if(pre == 0){
+            pre = 1;
+        }
+        if(suf == 0){
+            suf = 1;
+        }
         pre = pre*nums[i];
         suf = suf*nums[nums.size()-i-1];
         maxi = max(maxi, max(suf, pre));
