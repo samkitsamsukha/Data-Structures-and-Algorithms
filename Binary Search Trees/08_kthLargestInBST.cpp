d0fa3c124c96b28f9553ea09b7cb753b067c585a
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-01-29 00:51:23 +0530

    add kth smallest and kth largest in bst

---
+++ Binary Search Trees/08_kthLargestInBST.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+int ans = 0;
+
+void f(Node* root, int& k){
+    if(!root) return;
+    f(root->right, k);
+    k--;
+    if(k == 0){
+        ans = root->data;
+        return;
+    }
+    f(root->left, k);
+}
+
+int main()
+{
+    Node* root = new Node(10);
+    root->left = new Node(8);
+    root->right = new Node(12);
+    root->left->left = new Node(4);
+    root->left->right = new Node(9);
+    root->right->right = new Node(14);
+    
+    int k;
+    cout << "Enter value of k: ";
+    cin >> k;
+    f(root, k);
+    cout << ans << endl;
+
+    return 0;
+}
